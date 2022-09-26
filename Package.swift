9d33ff6a1401561f4c575e411815a317322461b4
Артём Скрипкин <70656234+Konskii@users.noreply.github.com>
2022-09-26 20:00:03 +0400

    fixed typo

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "6.0.0")),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git, .upToNextMajor(from: "5.0.0")")
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         .target(
