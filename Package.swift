e4cf9ad76c3a77747a286444eaf32642c2404197
Konskii <skripkin.skripkin2003@yandex.ru>
2022-09-26 20:25:45 +0400

    SPM Support

--- Package.swift
+++ Package.swift
@@ -5,22 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "MoyaSwiftyJSONMapper",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(
             name: "MoyaSwiftyJSONMapper",
-            targets: ["MoyaSwiftyJSONMapper"])
+            targets: ["MoyaSwiftyJSONMapper"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "6.0.0")),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .branch("master"))
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         .target(
             name: "MoyaSwiftyJSONMapper",
-            dependencies: [],
+            dependencies: [
+                "Moya",
+                "ReactiveSwift",
+                "RxSwift",
+                "SwiftyJSON"
+            ],
             path: "Source"
-            )
+        )
     ]
 )
