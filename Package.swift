1613cb752df94408f84800291beac90cc907ea7e
Konskii <skripkin.skripkin2003@yandex.ru>
2022-09-26 19:46:14 +0400

    add SPM support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MoyaSwiftyJSONMapper",
+    products: [
+        .library(
+            name: "MoyaSwiftyJSONMapper",
+            targets: ["MoyaSwiftyJSONMapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "6.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "MoyaSwiftyJSONMapper",
+            dependencies: []
+            )
+    ]
+)
